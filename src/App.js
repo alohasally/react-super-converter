03176bff29ccd488298c3a7c55b40bdd4f9b9ec7
sally <peacheeks@gmail.com>
2022-09-21 16:57:25 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [minutes, setMinutes] = useState();
+  const [amount, setAmount] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+
   const onChange = (event) => { 
-    setMinutes(event.target.value);
+    setAmount(event.target.value);
   };
+  const reset = () => setAmount(0);
+  const onFlip = () => {
+    reset();
+    setFlipped((current) => !current);
+  }
+
   return (
     <div >
       <h1 className='h1'>Super Converter</h1>
-        <label htmlFor='minutes'>Minutes</label>
-        <input 
-          value={minutes}
-          id='minutes'
-          placeholder='Minutes'
-          type='number'
-          onChange={onChange}
-          />
-          <h4>You want to convert {minutes} </h4>
+        <div>
+          <label htmlFor='minutes'>Minutes</label>
+          <input 
+            value={flipped ? Math.round(amount*60) : amount}
+            id='minutes'
+            placeholder='Minutes'
+            type='number'
+            onChange={onChange}
+            // disabled={flipped === true}
+            disabled={flipped}
+            />
+            <h4>You want to convert {amount} min </h4>
+        </div>
+        <div>
         <label htmlFor='hours'>Hours</label>
-        <input id='hours' placeholder='Hours' type='number'/>
+        <input 
+            value={flipped ? amount : Math.round(amount/60)} 
+            id='hours' 
+            placeholder='Hours' 
+            type='number'
+            onChange={onChange}
+            // disabled={flipped === false}
+            disabled={!flipped}
+            />
+        </div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={onFlip}>Flip</button>
     </div>
   );
 }
